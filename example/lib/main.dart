518a166ecda7733def5a478c6b35114c8235cf84
Fady <fedo.zaher@gmail.com>
2025-05-10 20:03:54 +0300

    reaction chat

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:fz_reactions_chat/fz_reactions_chat.dart';
+ import 'package:flutter/material.dart';
+ import 'package:fz_reactions_chat/fz_reactions_chat.dart';
 
 void main() {
   runApp(const MyApp());
